d26fb9e6b5f8bf25f9457a5c5e08f9e3e3f53a10
RedaKassoumeh123456 <kassoumehreda@gmail.com>
2025-05-15 14:00:45 +0300

    Update metadata in layout.jsx to enhance SEO and social sharing with Open Graph and Twitter card integration

--- app/layout.jsx
+++ app/layout.jsx
@@ -16,7 +16,28 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "Reda Kassoumeh | Software Engineer",
-  description: "Software Engineer",
+  description: "Explore my work and projects in full-stack web development.",
+  openGraph: {
+    title: "Reda Kassoumeh | Software Engineer",
+    description: "Explore my work and projects in full-stack web development.",
+    url: "https://redakassoumeh.vercel.app",
+    siteName: "Reda Kassoumeh Portfolio",
+    images: [
+      {
+        url: "/images/view-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Reda Kassoumeh Portfolio",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Reda Kassoumeh | Software Engineer",
+    description: "Explore my work and projects in full-stack web development.",
+    images: ["/images/view-image.png"],
+  },
 };
 
 export default function RootLayout({ children }) {
